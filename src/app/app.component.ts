76e991f3d19db0cd58871a92b05f6e115f95b98e
A4nold <37985346+A4nold@users.noreply.github.com>
2023-05-25 10:06:17 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { selectBookCollection, selectBook } from './state/books.selectors';
+import { BooksAction, BooksApiActions } from './state/book.actions';
+import { GoogleBooksService } from './book-list/books.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  books$ = this.store.select(selectBook);
+  bookCollection$ = this.store.select(selectBookCollection);
 
+  onAdd(bookId: string) {
+    this.store.dispatch(BooksAction.addBook({ bookId }));
+  }
 
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
+  onRemove(bookId: string) {
+    this.store.dispatch(BooksAction.removeBook({ bookId }));
+  }
+
+  constructor(private booksService: GoogleBooksService, private store: Store) {}
+
+  ngOnInit() {
+    this.booksService
+      .getBooks()
+      .subscribe((books) =>
+        this.store.dispatch(BooksApiActions.retrievedBookList({ books }))
+      );
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { booksReducer } from './state/books.reducer';
 import { collectionReducer } from './state/collection.reducer';
 import { StoreModule } from '@ngrx/store';
+
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
 
 @NgModule({
   imports: [
@@ -13,12 +16,7 @@ import { StoreModule } from '@ngrx/store';
     StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
     HttpClientModule,
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookListComponent, BookCollectionComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
